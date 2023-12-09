a11f39424311ce9302473a4d7fa26fdf743bf213
Maritedo <62166314+Maritedo@users.noreply.github.com>
2023-12-09 21:57:06 +0800

    bby

--- src/views/dev/undone/ColorGame.vue
+++ src/views/dev/undone/ColorGame.vue
@@ -25,9 +25,10 @@
         </n-layout-header>
         <n-layout-content>
             <div class="container">
-                <table class="grid" :style="autoStyle">
+                <table class="grid" :style="autoStyle" @click="onClickTable">
                     <tr v-for="x in arr" :key="x">
-                        <td v-for="y in arr" :key="y" :class="[paused ? 'paused' : (isTarget(x, y) ? 'target' : null)]">
+                        <td v-for="y in arr" :key="y" :val="x * size + y + 1"
+                            :class="[paused ? 'paused' : (isTarget(x, y) ? 'target' : null)]">
                         </td>
                     </tr>
                 </table>
@@ -44,7 +45,9 @@ const autoStyle = ref({
     "--normal": 'red',
     "--target": 'green'
 });
-const size = ref(3);
+const duration = 60;
+
+const size = computed(() => Math.min(Math.max(3, scores.value), 12));
 const n = ref(0);
 
 const scores = ref(0);
@@ -63,16 +66,19 @@ const arr = computed(() => {
     return result
 })
 const regenerate = () => {
-    const i = n.value;
-    while (n.value == i)
-        n.value = Math.ceil(Math.random() * size.value ** 2);
+    const temp = Math.ceil(Math.random() * (size.value ** 2 - 1));
+    n.value = temp < n.value ? temp : temp + 1;
 }
 
 const paused = ref(true);
 const initial = ref(true);
 
 const gameStart = () => {
-    initial.value = false;
+    if (initial.value) {
+        scores.value = 0;
+        initial.value = false;
+        regenerate();
+    }
     gameTimer.start();
     paused.value = false;
 }
@@ -84,6 +90,7 @@ const gamePause = () => {
 
 const gameRestart = () => {
     paused.value = true;
+    initial.value = true;
     updateDisp(duration * 1000)
     gameTimer.reset();
 }
@@ -93,8 +100,9 @@ function PrefixInteger(num, length) {
 }
 
 const gameEnd = () => {
-    n.value = -1;
-    alert('结束');
+    gameTimer.reset();
+    paused.value = true;
+    initial.value = true;
 }
 
 const updateDisp = (t) => {
@@ -102,22 +110,25 @@ const updateDisp = (t) => {
     remainTime.value = `${PrefixInteger(~~(seconds / 60), 2)}:${PrefixInteger(~~(seconds % 60), 2)}:${PrefixInteger(t % 1000, 3)}`
 }
 
-const duration = 60;
-const gameTimer = new Timer(
-    1000 * duration,
-    updateDisp,
-    gameEnd);
-console.log(gameTimer)
-
-onMounted(() => {
-    document.querySelector('table.grid').addEventListener('click', (e) => {
+const onClickTable = (e) => {
+    if (paused.value) {
+        gameStart();
+    } else {
         const t = e.target;
-        if (t.tagName == 'TD' && t.classList.contains('target')) {
+        if (t.tagName == 'TD' && t.getAttribute('val') == n.value) {
             scores.value++;
             if (scores.value > topScore.value) topScore.value = scores.value
             regenerate();
         }
-    });
+    }
+}
+
+const gameTimer = new Timer(
+    1000 * duration,
+    updateDisp,
+    gameEnd);
+
+onMounted(() => {
     updateDisp(duration * 1000);
     regenerate();
 })
@@ -133,7 +144,7 @@ onMounted(() => {
     display: block;
     width: 5em;
     padding: 0;
-    margin: auto 40px;
+    margin: auto 10px;
     height: 100%;
     line-height: 100%;
 }
@@ -146,20 +157,23 @@ onMounted(() => {
 
     table.grid {
         justify-self: center;
+        flex-direction: column;
         display: flex;
-        width: 300px;
-        height: 300px;
+        --len: min(70vw, 70vh);
+        width: var(--len);
+        height: var(--len);
 
         tr {
             display: flex;
-            flex-direction: column;
+            flex-direction: row;
             flex: 1;
 
 
             td {
+                display: block;
+                margin: 1.5px;
                 background-color: var(--normal);
                 flex: 1;
-                margin: 5%;
                 transition: background-color .05s linear;
 
                 &.target {
